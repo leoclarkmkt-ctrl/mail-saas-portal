6272762ed8fd6495f572f34e7adefed20865bdcc
leoclarkmkt-ctrl <leo.clark.mkt@gmail.com>
2026-02-10 19:24:23 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
   title: "NSUK Mail Portal",
   description: "NSUK official mail portal",
   icons: {
-    icon: [{ url: "/logo3.png", type: "image/pngl" }]
+    icon: [{ url: "/logo3.png", type: "image/png" }]
   }
 };
 
